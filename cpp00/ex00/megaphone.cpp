413bc6d8f531e55b4ab2ca86fbb53ab47163c9d7
Rodrigo Lins <rlinsdev@gmail.com>
2023-04-06 12:04:33 -0300

    Partial Megaphone

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,11 +6,11 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 15:31:19 by rlins             #+#    #+#             */
-/*   Updated: 2023/04/06 09:38:29 by rlins            ###   ########.fr       */
+/*   Updated: 2023/04/06 12:03:04 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main(int argc, char* argv[])
@@ -19,9 +19,22 @@ int main(int argc, char* argv[])
 	{
 		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
 	}
-	if (argv[1] == "Oi meu chapa!")
+	else
 	{
-		return (4);
+		string sentence = "";
+		for (int i = 1; i < argc; i++)
+		{
+			string word = argv[i];
+
+			transform(word.begin(), word.end(), word.begin(), ::toupper);
+			cout<<word<<endl;
+
+			if (sentence == "")
+				sentence += word;
+			else
+				sentence += " " + word;
+		}
+		cout << sentence;
 	}
 	return (0);
 }
