effff830685fda311fc389142a66dc1cef0ffb8d
Rodrigo Lins <rlinsdev@gmail.com>
2023-04-06 09:43:37 -0300

    CPP00 - ex00 - First part

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,12 +6,22 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 15:31:19 by rlins             #+#    #+#             */
-/*   Updated: 2023/04/04 15:51:24 by rlins            ###   ########.fr       */
+/*   Updated: 2023/04/06 09:38:29 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+using namespace std;
 
-int	main(int argc, char **argv)
+int main(int argc, char* argv[])
 {
-	return(argc);
+	if (argc == 1)
+	{
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+	}
+	if (argv[1] == "Oi meu chapa!")
+	{
+		return (4);
+	}
+	return (0);
 }
