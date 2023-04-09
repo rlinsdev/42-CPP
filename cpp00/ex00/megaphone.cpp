f408832bd44261b54cb01c3f4ffcb90ea7c0b440
Rodrigo Lins <rlinsdev@gmail.com>
2023-04-09 08:14:49 -0300

    Add std endl

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,18 +6,19 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 15:31:19 by rlins             #+#    #+#             */
-/*   Updated: 2023/04/06 12:23:28 by rlins            ###   ########.fr       */
+/*   Updated: 2023/04/09 08:00:28 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <bits/stdc++.h>
-// using namespace std;
+#include <sstream>		// std::stringstream
+#include <iostream> 	// std::cout
+#include <algorithm>	// std::transform
 
 int main(int argc, char *argv[])
 {
 	if (argc == 1)
 	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	}
 	else
 	{
@@ -33,8 +34,7 @@ int main(int argc, char *argv[])
 			else
 				sentence << " " << word;
 		}
-		std::cout << sentence.str();
-		std::cout << "\n";
+		std::cout << sentence.str() << std::endl;
 	}
 	return (0);
 }
