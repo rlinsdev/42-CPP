44ee1f180b034eda786436d249cc9eafc2749c51
Rodrigo Lins <rlinsdev@gmail.com>
2023-04-24 17:34:32 -0300

    Main  - temp

--- cpp00/ex01/sources/main.cpp
+++ cpp00/ex01/sources/main.cpp
@@ -6,14 +6,45 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 10:36:27 by rlins             #+#    #+#             */
-/*   Updated: 2023/04/24 10:51:57 by rlins            ###   ########.fr       */
+/*   Updated: 2023/04/24 12:44:45 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <PhoneBook.hpp>
 
 int	main(void)
 {
-	std::cout << "opa" << std::endl;
+	// std::cout << "opa" << std::endl;
+	PhoneBook pBook;
+	pBook.menu();
+
+	std::string value;
+	while (true)
+	{
+		std::cout << "> ";
+		getline(std::cin, value);
+
+		if (value == "1" || value = "ADD")
+		{
+			std::cout << "1" << std::endl;
+		}
+		else if (value == "2" || value == "SEARCH")
+		{
+			std::cout << "2" << std::endl;
+		}
+		else if (value == "3" || value == "EXIT")
+		{
+			break ;
+		}
+		else
+		{
+			std::cout << "Invalid Choice. Try again" << std::endl;
+		}
+	}
+	std::cout << "|------------------------------------------|" << std::endl;
+	std::cout << "|-------- Welcome to PhoneBook(42) --------|" << std::endl;
+	std::cout << "|----------- Choose one option: -----------|" << std::endl;
+	std::cout << "|------------------------------------------|" << std::endl;
 	return (0);
 }
