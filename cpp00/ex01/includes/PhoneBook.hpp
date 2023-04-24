d00f7b453e431a2e16f08aa904b075717afa5107
Rodrigo Lins <rlinsdev@gmail.com>
2023-04-24 17:33:43 -0300

    Phone book header

---
+++ cpp00/ex01/includes/PhoneBook.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/24 11:01:55 by rlins             #+#    #+#             */
+/*   Updated: 2023/04/24 11:55:54 by rlins            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_H
+# define PHONEBOOK_H
+
+class PhoneBook
+{
+	private:
+		/* data */
+	public:
+		PhoneBook();
+		~PhoneBook();
+		void	menu();
+};
+
+#endif
+
+
+
