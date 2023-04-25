c5c18c4e1cd8231c222efb079dc1116acd5d25b7
Rodrigo Lins <rlinsdev@gmail.com>
2023-04-25 08:23:08 -0300

    Contact Class - Getter setter

--- cpp00/ex01/includes/Contact.hpp
+++ cpp00/ex01/includes/Contact.hpp
@@ -7,13 +7,26 @@ class Contact
 {
 	private:
 		std::string	_firstName;
+		std::string _lastName;
+		std::string _nickName;
+		std::string _phoneNumber;
+		std::string _darkestSecret;
 
 	public:
 		Contact();
 		~Contact();
 
-		std::string const	getFirstName(void) const;
-		bool				setFirstName(std::string str);
+		std::string		getFirstName(void) const;
+		std::string		getLastName(void) const;
+		std::string		getNickName(void) const;
+		std::string		getPhoneNumber(void) const;
+		std::string		getDarkestSecret(void) const;
+
+		void			setFirstName(const std::string firstName);
+		void			setLastName(const std::string lastName);
+		void			setNickName(const std::string nickName);
+		void			setPhoneNumber(const std::string phoneNumber);
+		void			setDarkestSecret(const std::string darkestSecret);
 };
 
 #endif
--- cpp00/ex01/sources/Contact.cpp
+++ cpp00/ex01/sources/Contact.cpp
@@ -6,18 +6,61 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 18:18:10 by rlins             #+#    #+#             */
-/*   Updated: 2023/04/24 18:20:54 by rlins            ###   ########.fr       */
+/*   Updated: 2023/04/25 08:22:35 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Contact.hpp"
 
-
-
+/**
+ * @brief Construct a new Contact:: Contact object
+ */
 Contact::Contact()
 {
 }
 
+/**
+ * @brief Destructor of Contact object
+ */
 Contact::~Contact()
 {
+}
+
+/**
+ * @brief Getters of Contact class
+ */
+
+std::string	Contact::getFirstName(void) const {
+	return (this->_firstName);
+}
+std::string Contact::getLastName(void) const {
+	return (this->_lastName);
+}
+std::string Contact::getNickName(void) const {
+	return (this->_nickName);
+}
+std::string Contact::getPhoneNumber(void) const {
+	return (this->_phoneNumber);
+}
+std::string Contact::getDarkestSecret(void) const {
+	return (this->_darkestSecret);
+}
+
+/**
+ * @brief Setters of Contact class
+ */
+void Contact::setFirstName(const std::string firstName) {
+	this->_firstName = firstName;
+}
+void Contact::setLastName(const std::string lastName) {
+	this->_lastName = lastName;
+}
+void Contact::setNickName(const std::string  nickName) {
+	this->_nickName = nickName;
+}
+void Contact::setPhoneNumber(const std::string phoneNumber) {
+	this->_phoneNumber = phoneNumber;
+}
+void Contact::setDarkestSecret(const std::string darkestSecret) {
+	this->_darkestSecret = darkestSecret;
 }
