25a5de69931927ed8a760d98e9e4c3bc6ab07e75
Rodrigo Lins <rlinsdev@gmail.com>
2023-04-26 08:42:39 -0300

    Formatting main

--- cpp00/ex01/sources/main.cpp
+++ cpp00/ex01/sources/main.cpp
@@ -6,39 +6,29 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 10:36:27 by rlins             #+#    #+#             */
-/*   Updated: 2023/04/25 16:19:02 by rlins            ###   ########.fr       */
+/*   Updated: 2023/04/26 08:42:28 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <PhoneBook.hpp>
 
-int	main(void)
-{
-	// std::cout << "opa" << std::endl;
+int	main(void) {
 	PhoneBook pBook;
 
 	std::string value;
-	while (true)
-	{
+	while (true) {
 		pBook.menu();
 		std::cout << "> ";
 		getline(std::cin, value);
 
-		if (value == "1" || value == "ADD")
-		{
+		if (value == "1" || value == "ADD") {
 			pBook.addContact();
-		}
-		else if (value == "2" || value == "SEARCH")
-		{
+		} else if (value == "2" || value == "SEARCH") {
 			pBook.searchContact();
-		}
-		else if (value == "3" || value == "EXIT")
-		{
+		} else if (value == "3" || value == "EXIT") {
 			break ;
-		}
-		else
-		{
+		} else {
 			std::cout << "Invalid Choice. Try again" << std::endl;
 		}
 	}
