27473479973baf8d16d09a4485b4cfaac1fbfc04
Rodrigo Lins <rlinsdev@gmail.com>
2023-04-27 17:08:26 -0300

    Refactoring ex 00

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,28 +6,27 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 15:31:19 by rlins             #+#    #+#             */
-/*   Updated: 2023/04/27 12:32:12 by rlins            ###   ########.fr       */
+/*   Updated: 2023/04/27 17:05:53 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <sstream>		// std::stringstream
 #include <iostream> 	// std::cout
-#include <algorithm>	// std::transform
 
 int main(int argc, char *argv[]) {
 	if (argc == 1) {
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
-	std::stringstream sentence;
 	for (int i = 1; i < argc; i++) {
-		std::string word = argv[i];
-		std::transform(word.begin(), word.end(), word.begin(), ::toupper);
-		if (sentence.str() == "")
-			sentence << word;
-		else
-			sentence << " " << word;
+		std::string word(argv[i]);
+		for (std::string::iterator it = word.begin(); it != word.end(); it++) {
+			*it = std::toupper(*it);
+		}
+		std::cout << word;
+		if (i + 1 < argc) {
+			std::cout << " ";
+		}
 	}
-	std::cout << sentence.str() << std::endl;
+	std::cout << std::endl;
 	return (0);
 }
