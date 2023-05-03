b2cf060528d70b802a6b37c8c950280a6fa9c7e8
Rodrigo Lins <rlinsdev@gmail.com>
2023-05-03 07:39:34 -0300

    fixing zombie impl

--- cpp01/ex00/sources/Zombie.cpp
+++ cpp01/ex00/sources/Zombie.cpp
@@ -6,11 +6,13 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 08:33:06 by rlins             #+#    #+#             */
-/*   Updated: 2023/05/03 07:06:46 by rlins            ###   ########.fr       */
+/*   Updated: 2023/05/03 07:39:04 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <Zombie.hpp>
+#include <string>
+#include <iostream>
 
 /**
  * @brief Constructor
@@ -45,6 +47,10 @@ std::string Zombie::getName(void) const {
 	return (this->_name);
 }
 
+/**
+ * @brief Zombie announce itself
+ */
 void	Zombie::announce(void) {
-	std::cout << this->getName << ": BraiiiiiiinnnzzzZ..." << std::endl;
+	std::cout << this->getName() << ": BraiiiiiiinnnzzzZ..." << std::endl;
+	return ;
 }
