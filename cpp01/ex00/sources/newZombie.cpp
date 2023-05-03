2cd11dbb0e2d8f91f228ece96642fcea36c4e98f
Rodrigo Lins <rlinsdev@gmail.com>
2023-05-03 07:40:05 -0300

    new Zombie impl

---
+++ cpp01/ex00/sources/newZombie.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   newZombie.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/03 06:37:30 by rlins             #+#    #+#             */
+/*   Updated: 2023/05/03 07:32:51 by rlins            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <Zombie.hpp>
+
+/**
+ * @brief It creates a zombie, name it, and return it so you can use it outside
+ * of the function scope
+ * @param name
+ * @return Zombie*
+ */
+Zombie *newZombie(std::string name) {
+	Zombie *zombie = new Zombie(name);
+	zombie->announce();
+	return(zombie);
+}
