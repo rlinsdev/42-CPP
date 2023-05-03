26453453ba3df606d42ffdd67e88c78c2b61bea5
Rodrigo Lins <rlinsdev@gmail.com>
2023-05-03 08:28:26 -0300

    Main impl

--- cpp01/ex00/sources/main.cpp
+++ cpp01/ex00/sources/main.cpp
@@ -6,11 +6,24 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 08:05:14 by rlins             #+#    #+#             */
-/*   Updated: 2023/05/02 08:05:21 by rlins            ###   ########.fr       */
+/*   Updated: 2023/05/03 08:27:55 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(void) {
+#include <iostream>
+#include <string>
+#include "Zombie.hpp"
 
+Zombie	*newZombie(std::string name);
+void	randomChump(std::string name);
+
+int	main(void) {
+	// Heap Allocation
+	Zombie *firstZombie = newZombie("Zomb-1");
+	firstZombie->announce();
+	delete firstZombie;
+
+	// Stack allocation
+	randomChump("Zomb-2");
 	return (0);
 }
