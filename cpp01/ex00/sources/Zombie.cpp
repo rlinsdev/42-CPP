e3244370050477ed6f6477a8955539b71504d0bd
Rodrigo Lins <rlinsdev@gmail.com>
2023-05-03 07:14:31 -0300

    Zombie implementation

--- cpp01/ex00/sources/Zombie.cpp
+++ cpp01/ex00/sources/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 08:33:06 by rlins             #+#    #+#             */
-/*   Updated: 2023/05/02 08:39:22 by rlins            ###   ########.fr       */
+/*   Updated: 2023/05/03 07:06:46 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,36 @@
 /**
  * @brief Constructor
  */
-Zombie::Zombie() {
+Zombie::Zombie(void) {
+	std::cout << "A zombie respawn!" << std::endl;
+	return ;
+}
+
+/**
+ * @brief Construct a new Zombie: name by param
+ * @param name
+ */
+Zombie::Zombie(std::string name) : _name(name) {
+	std::cout << "The zombie " << this->getName() << " respawn!" << std::endl;
+	return ;
 }
 
 /**
  * @brief Destructor
  */
-Zombie::~Zombie() {
+Zombie::~Zombie(void) {
+	std::cout << "No human found. I'll fall to sleep again!" << std::endl;
+	return ;
+}
+
+/**
+ * @brief Getter - Get Name
+ * @return std::string
+ */
+std::string Zombie::getName(void) const {
+	return (this->_name);
+}
+
+void	Zombie::announce(void) {
+	std::cout << this->getName << ": BraiiiiiiinnnzzzZ..." << std::endl;
 }
