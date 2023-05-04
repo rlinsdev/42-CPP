ee5b4bfe91c269e9e3b9c6b76aafcf3f9f0ae448
Rodrigo Lins <rlinsdev@gmail.com>
2023-05-04 07:25:57 -0300

    input validation

--- cpp01/ex01/sources/main.cpp
+++ cpp01/ex01/sources/main.cpp
@@ -6,24 +6,42 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 08:05:14 by rlins             #+#    #+#             */
-/*   Updated: 2023/05/03 08:27:55 by rlins            ###   ########.fr       */
+/*   Updated: 2023/05/04 07:24:58 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <limits> 		// max
 #include <string>
 #include "Zombie.hpp"
 
-Zombie	*newZombie(std::string name);
-void	randomChump(std::string name);
+#define MAX_HORDE 10000
+#define MIN_HORDE -10000
+
+Zombie *zombieHorde(int N, std::string name);
+
+int	handleZombieNumber(bool firstCall) {
+	int	nb = 0;
+
+	if (firstCall == true) {
+		std::cout << "Put the number of the Hordes of Zombies: ";
+	}
+	std::cout << std::endl << "> ";
+	std::cin >> nb;
+	while (!(std::cin >> nb) || (nb < MIN_HORDE || nb > MAX_HORDE)) {
+		std::cout << "Invalid input. Try something between '"<< MIN_HORDE << "' and '" << MAX_HORDE <<"'";
+		std::cout << std::endl << "> ";
+
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	}
+	return (nb);
+}
 
 int	main(void) {
-	// Heap Allocation
-	Zombie *firstZombie = newZombie("Zomb-1");
-	firstZombie->announce();
-	delete firstZombie;
+	int nb = handleZombieNumber(true);
 
-	// Stack allocation
-	randomChump("Zomb-2");
+	std::cout << nb;
+
 	return (0);
 }
