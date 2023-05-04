92352f7ec94c1f829b17334db3861cf911199862
Rodrigo Lins <rlinsdev@gmail.com>
2023-05-04 08:14:12 -0300

    main implementation

--- cpp01/ex01/sources/main.cpp
+++ cpp01/ex01/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 08:05:14 by rlins             #+#    #+#             */
-/*   Updated: 2023/05/04 07:24:58 by rlins            ###   ########.fr       */
+/*   Updated: 2023/05/04 08:11:32 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,15 @@
 
 Zombie *zombieHorde(int N, std::string name);
 
-int	handleZombieNumber(bool firstCall) {
+/**
+ * @brief Get the number of Horde. Limited by previous definition and handle
+ * not numeric input
+ * @return int
+ */
+int handleZombieNumber() {
 	int	nb = 0;
 
-	if (firstCall == true) {
-		std::cout << "Put the number of the Hordes of Zombies: ";
-	}
-	std::cout << std::endl << "> ";
-	std::cin >> nb;
+	std::cout << "Put the number of the Hordes of Zombies: " << std::endl << "> ";
 	while (!(std::cin >> nb) || (nb < MIN_HORDE || nb > MAX_HORDE)) {
 		std::cout << "Invalid input. Try something between '"<< MIN_HORDE << "' and '" << MAX_HORDE <<"'";
 		std::cout << std::endl << "> ";
@@ -38,10 +39,32 @@ int	handleZombieNumber(bool firstCall) {
 	return (nb);
 }
 
+/**
+ * @brief Get a Horde name
+ * @return string
+ */
+std::string	getHordeName(void) {
+	std::string	name;
+
+	std::cin.ignore();
+	std::cout << "Give a name to your squad:" << std::endl << "> ";
+	std::getline(std::cin, name);
+	return (name);
+}
+
 int	main(void) {
-	int nb = handleZombieNumber(true);
+	int nb = handleZombieNumber();
+	std::string squadName = getHordeName();
+	std::cout << "---------------" << std::endl;
+	std::cout << "Summoning the horde of " << nb << " zombies..." << std::endl;
 
-	std::cout << nb;
+	Zombie *allHorde = zombieHorde(nb, squadName);
 
+	for (int i = 0; i < nb; i++) {
+		allHorde[i].announce();
+	}
+	std::cout << "---------------" << std::endl;
+	std::cout << "Return to your graves Zombies!" << std::endl;
+	delete[] allHorde;
 	return (0);
 }
