778a2ac6f5891bbbeff584400ab2354dc1d6f17f
Rodrigo Lins <rlinsdev@gmail.com>
2023-05-05 07:30:30 -0300

    Basic structure

---
+++ cpp01/ex03/includes/HumanB.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HumanB.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/05 07:19:31 by rlins             #+#    #+#             */
+/*   Updated: 2023/05/05 07:20:48 by rlins            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HUMAN_B_CLASS_H
+# define HUMAN_B_CLASS_H
+
+# include "Weapon.hpp"
+
+class HumanB {
+	private:
+		/* data */
+	public:
+		HumanB(/* args */);
+		~HumanB();
+};
+
+#endif
