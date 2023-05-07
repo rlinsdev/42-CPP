acef3cb4fd5d3244a4b936714af18f338edb30b9
Rodrigo Lins <rlinsdev@gmail.com>
2023-05-07 08:45:25 -0300

    Main from pdf

--- cpp01/ex03/sources/main.cpp
+++ cpp01/ex03/sources/main.cpp
@@ -6,14 +6,34 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 06:55:30 by rlins             #+#    #+#             */
-/*   Updated: 2023/05/05 06:56:59 by rlins            ###   ########.fr       */
+/*   Updated: 2023/05/07 08:45:18 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <string>
+# include <iostream>
+# include <string>
+# include <Weapon.hpp>
+# include <HumanA.hpp>
+# include <HumanB.hpp>
 
 int	main(void) {
+
+		// Human A attack:
+		Weapon club = Weapon("crude spiked club");
+
+		HumanA bob("Bob", club);
+		bob.attack();
+		club.setType("some other type of club");
+		bob.attack();
+
+		// Human B attack:
+		Weapon club2 = Weapon("crude spiked club");
+
+		HumanB jim("Jim");
+		jim.setWeapon(club2);
+		jim.attack();
+		club2.setType("some other type of club");
+		jim.attack();
 
 	return (0);
 }
