954a1505645564994d6a418c3342043a12a167ab
Rodrigo Lins <rlinsdev@gmail.com>
2023-05-07 08:44:41 -0300

    attack implementation

--- cpp01/ex03/sources/HumanA.cpp
+++ cpp01/ex03/sources/HumanA.cpp
@@ -6,7 +6,7 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 07:17:15 by rlins             #+#    #+#             */
-/*   Updated: 2023/05/05 08:21:31 by rlins            ###   ########.fr       */
+/*   Updated: 2023/05/07 08:31:27 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,21 @@
 #include <iostream>
 
 HumanA::HumanA(std::string const aName, Weapon const aWeapon)
-	: _name( aName ),	_weapon( aWeapon ) {
+	: _name( aName ), _weapon( aWeapon ) {
 }
 
-HumanA::~HumanA() {
+HumanA::~HumanA(void) {
 }
 
 /**
  * @brief Attack method
  */
 void	HumanA::attack( void ) const {
-	std::cout << "Attackk A";
+	std::cout << this->_name << " attack with their ";
+	if (this->_weapon.getType() == "") {
+		std::cout << "hands!";
+	} else {
+		std::cout << this->_weapon.getType() << "!";
+	}
+	return ;
 }
