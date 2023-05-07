208fb44abeae3101c8988a58570c8c137f1f597f
Rodrigo Lins <rlinsdev@gmail.com>
2023-05-07 08:44:22 -0300

    Set Weapon and attack implementation

--- cpp01/ex03/includes/HumanB.hpp
+++ cpp01/ex03/includes/HumanB.hpp
@@ -6,7 +6,7 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 07:19:31 by rlins             #+#    #+#             */
-/*   Updated: 2023/05/05 08:19:46 by rlins            ###   ########.fr       */
+/*   Updated: 2023/05/07 08:40:32 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,14 @@
 class HumanB {
 	private:
 		std::string const	_name;
-		Weapon const 		_weapon;
+		Weapon const 		*_weapon;
 
 	public:
 		HumanB(std::string const bName);
 		~HumanB();
 
-		void	attack( void ) const;
+		void	attack(void) const;
+		void	setWeapon(Weapon &aWeapon);
 };
 
 #endif
--- cpp01/ex03/sources/HumanB.cpp
+++ cpp01/ex03/sources/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 07:20:26 by rlins             #+#    #+#             */
-/*   Updated: 2023/05/05 08:23:44 by rlins            ###   ########.fr       */
+/*   Updated: 2023/05/07 08:39:38 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,26 @@ HumanB::~HumanB() {
 /**
  * @brief Attack method
  */
-void	HumanB::attack( void ) const {
-	std::cout << "Attackk B";
+void	HumanB::attack(void) const {
+	std::cout << this->_name << " attack with their ";
+	if (this->_weapon != NULL && this->_weapon->getType() != "") {
+		std::cout << this->_weapon->getType() << ".";
+ 	} else {
+		std::cout << "fists!";
+	}
+	std::cout << std::endl;
+	return ;
+}
+
+/**
+ * @brief
+ * @param aWeapon
+ */
+void	HumanB::setWeapon(Weapon &aWeapon) {
+	if (aWeapon.getType().empty()) {
+		this->_weapon = NULL;
+	} else {
+		this->_weapon = &aWeapon;
+	}
+	return ;
 }
