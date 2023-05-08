992fdddf54d9fd5b396ceb98a3712f8fc420ca5b
Rodrigo Lins <rlinsdev@gmail.com>
2023-05-08 08:14:43 -0300

    complain calls

--- cpp01/ex05/sources/main.cpp
+++ cpp01/ex05/sources/main.cpp
@@ -6,14 +6,19 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 08:33:13 by rlins             #+#    #+#             */
-/*   Updated: 2023/05/08 07:17:42 by rlins            ###   ########.fr       */
+/*   Updated: 2023/05/08 08:13:58 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <Harl.hpp>
 
 int main(void) {
+	Harl harl;
 
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
 
 	return (0);
 }
