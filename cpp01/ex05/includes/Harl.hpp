4454ad81a518c3e5b090a8201db63507a2c12b89
Rodrigo Lins <rlinsdev@gmail.com>
2023-05-08 07:59:12 -0300

    Header

--- cpp01/ex05/includes/Harl.hpp
+++ cpp01/ex05/includes/Harl.hpp
@@ -6,14 +6,31 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 06:56:18 by rlins             #+#    #+#             */
-/*   Updated: 2023/05/08 06:56:54 by rlins            ###   ########.fr       */
+/*   Updated: 2023/05/08 07:57:15 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #ifndef HARL_H
 # define HARL_H
 
+#include <iostream>
+# include <string>
+
+class Harl {
+	private:
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+
+	public:
+		Harl(void);
+		~Harl();
+
+		void	complain(std::string level);
+};
+
+typedef void (Harl::*t_func) (void);
 
 #endif
 
