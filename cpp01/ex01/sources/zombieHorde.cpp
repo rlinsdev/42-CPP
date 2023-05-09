8490516953bb6fb615fd08be5f9fbf17eab2dac4
Rodrigo Lins <rlinsdev@gmail.com>
2023-05-09 08:52:35 -0300

    Add doc in zombie horde

--- cpp01/ex01/sources/zombieHorde.cpp
+++ cpp01/ex01/sources/zombieHorde.cpp
@@ -6,12 +6,18 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 12:14:36 by rlins             #+#    #+#             */
-/*   Updated: 2023/05/04 08:20:43 by rlins            ###   ########.fr       */
+/*   Updated: 2023/05/09 08:48:54 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
+/**
+ * @brief Create a Horde of zombies
+ * @param N Number of zombies
+ * @param name Name of squad
+ * @return Zombie*
+ */
 Zombie *zombieHorde(int N, std::string name) {
 	if (N < 1) {
 		return (NULL);
