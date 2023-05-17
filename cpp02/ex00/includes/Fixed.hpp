dc52f036753fa425b52c43954c006d45b4551f5d
Rodrigo Lins <rlinsdev@gmail.com>
2023-05-17 08:30:36 -0300

    Header

--- cpp02/ex00/includes/Fixed.hpp
+++ cpp02/ex00/includes/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 06:14:19 by rlins             #+#    #+#             */
-/*   Updated: 2023/05/17 06:17:38 by rlins            ###   ########.fr       */
+/*   Updated: 2023/05/17 06:23:04 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,7 @@
 
 class Fixed {
 	private:
-		// std::string _name;
-		// std::string getName(void) const;
+		int		_fixed_point_value;
 
 	public:
 		Fixed(void);
