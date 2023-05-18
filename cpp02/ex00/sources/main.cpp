32d5235f36c85c3a287304108f10156b4e346ff1
Rodrigo Lins <rlinsdev@gmail.com>
2023-05-18 08:30:51 -0300

    Add code from PDF

--- cpp02/ex00/sources/main.cpp
+++ cpp02/ex00/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 06:18:49 by rlins             #+#    #+#             */
-/*   Updated: 2023/05/17 06:19:17 by rlins            ###   ########.fr       */
+/*   Updated: 2023/05/18 07:55:30 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,17 @@
 #include <string>
 #include "Fixed.hpp"
 
-// Zombie	*newZombie(std::string name);
-// void	randomChump(std::string name);
-
 int	main(void) {
-	std::cout  << "olá amundo" << std::endl;
-	// // Heap Allocation
-	// Zombie *firstZombie = newZombie("Zomb-1");
-	// firstZombie->announce();
-	// delete firstZombie;
+	// Code from PDF
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
 
-	// // Stack allocation
-	// randomChump("Zomb-2");
+	c = b;
+
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+
 	return (0);
 }
