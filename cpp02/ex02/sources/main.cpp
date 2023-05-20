7f461e842b131313ed4915ccf3737c5e232c6edf
Rodrigo Lins <rlinsdev@gmail.com>
2023-05-20 18:06:16 -0300

    Subject Code!

--- cpp02/ex02/sources/main.cpp
+++ cpp02/ex02/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 06:18:49 by rlins             #+#    #+#             */
-/*   Updated: 2023/05/20 17:05:00 by rlins            ###   ########.fr       */
+/*   Updated: 2023/05/20 18:05:53 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,18 @@
 #include "Fixed.hpp"
 
 int	main(void) {
-	// Code from PDF
+	// Subject Code
 
-	Fixed a;
-	Fixed const b(0);
-	Fixed const c(42.42f);
-	Fixed const d(b);
-	a = Fixed( 1234.4321f );
-	std::cout << "a is " << a << std::endl;
-	std::cout << "b is " << b << std::endl;
-	std::cout << "c is " << c << std::endl;
-	std::cout << "d is " << d << std::endl;
-	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	Fixed	a;
+	Fixed	const b(Fixed(5.05f) * Fixed(2));
+
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << Fixed::max(a, b) << std::endl;
 
 	return (0);
 }
