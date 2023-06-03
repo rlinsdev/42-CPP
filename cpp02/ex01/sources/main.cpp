fbf52bf862c4d541ceae1f321b52fc6ff99a5af7
Rodrigo Lins <rlinsdev@gmail.com>
2023-06-03 08:25:43 -0300

    let the same sample in main

--- cpp02/ex01/sources/main.cpp
+++ cpp02/ex01/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 06:18:49 by rlins             #+#    #+#             */
-/*   Updated: 2023/05/20 17:05:00 by rlins            ###   ########.fr       */
+/*   Updated: 2023/06/03 08:24:08 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	main(void) {
 	// Code from PDF
 
 	Fixed a;
-	Fixed const b(0);
+	Fixed const b(10);
 	Fixed const c(42.42f);
 	Fixed const d(b);
 	a = Fixed( 1234.4321f );
