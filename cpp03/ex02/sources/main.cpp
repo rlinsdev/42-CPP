19f6a3d08e1f61d02e7b80cce48dae3b55e0a5e6
Rodrigo Lins <rlinsdev@gmail.com>
2023-06-08 10:55:46 -0300

    CPP03 - ex02 -First Commit

---
+++ cpp03/ex02/sources/main.cpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/04 07:40:47 by rlins             #+#    #+#             */
+/*   Updated: 2023/06/08 09:45:56 by rlins            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <ClapTrap.hpp>
+#include <ScavTrap.hpp>
+#include <iostream>
+
+int	main(void) {
+
+	std::string rb1 = "Robot-01";
+	std::string rb2 = "Android-02";
+
+	ClapTrap ct1 (rb1);
+	ScavTrap ct2 (rb2);
+
+	std::cout << std::endl << "Test: " << rb1 << " Attack" << std::endl;
+	ct1.setAttackDamage(1);
+	ct1.attack(rb2);
+	ct2.takeDamage(ct1.getAttackDamage());
+
+	std::cout << std::endl << "Test: " << rb2 << " Attack" << std::endl;
+	ct2.setAttackDamage(2);
+	ct2.attack(rb1);
+	ct1.takeDamage(ct2.getAttackDamage());
+
+	std::cout << std::endl << "\tGuardGate Test" << std::endl;
+	ct2.guardGate();
+
+	std::cout << std::endl << "Test: Repaired all" << std::endl;
+	ct1.beRepaired(2);
+	ct2.beRepaired(1);
+
+	std::cout << std::endl << "End of simulation..." << std::endl;
+
+	return (0);
+}
