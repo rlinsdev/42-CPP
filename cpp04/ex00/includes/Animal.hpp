fe358b56963832eeb9ea2eec8e8554233efdb742
Rodrigo Lins <rlinsdev@gmail.com>
2023-06-09 07:41:36 -0300

    Cpp04 - First Commit

---
+++ cpp04/ex00/includes/Animal.hpp
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/09 07:38:34 by rlins             #+#    #+#             */
+/*   Updated: 2023/06/09 07:38:53 by rlins            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef ANIMAL_H
+# define ANIMAL_H
+
+# include <string>
+
+class Animal {
+	private:
+		// ClapTrap(void);
+		// std::string		_name;
+		// unsigned int	_hitPoints;
+		// unsigned int	_energyPoints;
+		// unsigned int	_attackDamage;
+
+	public:
+
+		// // Static Constants
+		// static int const	c_hitPoints = 10;
+		// static int const	c_energyPoints = 10;
+		// static int const	c_attackDamagePoints = 0;
+
+		// // Constructor and destructor
+		// ClapTrap(std::string name);
+		// ClapTrap(ClapTrap const & src);
+		// ~ClapTrap(void);
+
+		// // Operator by Orthodox Canonical Form
+		// ClapTrap &	operator=(ClapTrap const & src);
+
+		// // Getters and setters
+		// std::string		getName(void) const;
+		// unsigned int	getHitPoints(void) const;
+		// unsigned int	getEnergyPoints(void) const;
+		// unsigned int	getAttackDamage(void) const;
+		// void			setAttackDamage(unsigned int value);
+
+		// // Public members from subject
+		// void		attack(std::string & target);
+		// void		takeDamage(unsigned int amount);
+		// void		beRepaired(unsigned int amount);
+	};
+
+#endif
---
+++ cpp04/ex00/sources/Animal.cpp
@@ -0,0 +1,162 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/09 07:39:10 by rlins             #+#    #+#             */
+/*   Updated: 2023/06/09 07:40:14 by rlins            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <Animal.hpp>
+#include <string>
+#include <iostream>
+
+/**
+ * @brief Default Constructor
+ */
+// ClapTrap::ClapTrap(void) :
+// 	_name("unnamed"),
+// 	_hitPoints(ClapTrap::c_hitPoints),
+// 	_energyPoints(ClapTrap::c_energyPoints),
+// 	_attackDamage(ClapTrap::c_attackDamagePoints) {
+// 	std::cout << "Cl4p-tp with no name was constructed" << std::endl;
+// 	return ;
+// }
+
+/**
+ * @brief Default constructor - Name by param
+ * @param name
+ */
+// ClapTrap::ClapTrap(std::string name) :
+// 	_name(name),
+// 	_hitPoints(ClapTrap::c_hitPoints),
+// 	_energyPoints(ClapTrap::c_energyPoints),
+// 	_attackDamage(ClapTrap::c_attackDamagePoints) {
+// 	std::cout << "Cl4p-tp named **" << name << " was constructed" << std::endl;
+// 	return ;
+// }
+
+/**
+ * @brief Copy constructor
+ */
+// ClapTrap::ClapTrap(ClapTrap const & src) {
+// 	std::cout << "Copy constructor called." << std::endl;
+// 	*this = src;
+// 	return ;
+// }
+
+/**
+ * @brief Destructor
+ */
+// ClapTrap::~ClapTrap(void) {
+// 	std::cout << "Cl4p-tp **" << this->_name << " destroyed" << std::endl;
+// 	return ;
+// }
+
+/**
+ * @brief Copy assignment operator
+ */
+// ClapTrap & ClapTrap::operator=(ClapTrap const & src) {
+// 	if (this != &src) {
+// 		this->_name = src.getName();
+
+// 	}
+// 	return (*this);
+// }
+
+/******************************************************************************/
+/*BEGIN - Getters and Setters*/
+/******************************************************************************/
+// std::string ClapTrap::getName(void) const {
+// 	return (this->_name);
+// }
+// unsigned int ClapTrap::getHitPoints(void) const {
+// 	return (this->_hitPoints);
+// }
+// unsigned int ClapTrap::getEnergyPoints(void) const {
+// 	return (this->_energyPoints);
+// }
+// unsigned int ClapTrap::getAttackDamage(void) const {
+// 	return (this->_attackDamage);
+// }
+// void ClapTrap::setAttackDamage(unsigned int value) {
+// 	this->_attackDamage = value;
+// }
+/******************************************************************************/
+/*END -Getters and Setters*/
+/******************************************************************************/
+
+/**
+ * @brief When ClapTrack attacks, it causes its target to lose <attack damage> hit points.
+ * Attacking and repairing cost 1 energy point each.
+ * Of course, ClapTrap can’t do anything if it has no hit points or energy points left.
+ * @param target
+ */
+// void		ClapTrap::attack(std::string & target) {
+// 	if (this->_hitPoints <= 0) {
+// 		std::cout << "No more Hit Points to attack..." << std::endl;
+// 		return ;
+// 	}
+
+// 	if (this->_energyPoints <= 0) {
+// 		std::cout << "No more energy to attack..." << std::endl;
+// 		return ;
+// 	}
+
+// 	this->_energyPoints--;
+// 	std::cout << "Cl4p-tp **" << this->_name << " attacks **" << target
+// 		<< ", causing " << this->_attackDamage << " points of damage! Energy Remain: " << this->_energyPoints << "." << std::endl;
+// }
+
+/**
+ * @brief Take damage will will loose hit points
+ * @param amount
+ */
+// void		ClapTrap::takeDamage(unsigned int amount) {
+
+// 	if (amount <= 0) {
+// 		std::cout << "Invalid Amount to Take Damage" << std::endl;
+// 		return ;
+// 	}
+
+// 	if (this->_hitPoints <= 0) {
+// 		std::cout << "No more Hit Points to take damage..." << std::endl;
+// 		return ;
+// 	}
+
+// 	this->_hitPoints -= amount;
+// 	std::cout << "Cl4p-tp **" << this->_name << " takes " << amount << " damage... Heal remain: "
+// 		<< this->_hitPoints << "." << std::endl;
+// }
+/**
+ * @brief When ClapTrap repairs itself, it gets <amount> hit points back
+ * Attacking and repairing cost 1 energy point each.
+ * Of course, ClapTrap can’t do anything if it has no hit points or energy points left.
+ * @param amount
+ */
+// void	ClapTrap::beRepaired(unsigned int amount) {
+
+// 	if (amount <= 0) {
+// 		std::cout << "Invalid Amount to repair something..." << std::endl;
+// 		return ;
+// 	}
+
+// 	if (this->_hitPoints <= 0) {
+// 		std::cout << "Cl4p-tp **" << this->_name << " can't repaired... HitPoints Required..." << std::endl;
+// 		return ;
+// 	}
+
+// 	if (this->_energyPoints <= 0) {
+// 		std::cout << "Cl4p-tp **" << this->_name << " can't repaired... EnergyPoints Required..." << std::endl;
+// 		return ;
+// 	}
+
+// 	this->_energyPoints--;
+// 	this->_hitPoints += amount;
+// 	std::cout << "Cl4p-tp **" << this->_name << " repairs " << amount << ", healing: "
+// 		<< this->_hitPoints << "." << std::endl;
+// 	return ;
+// }
