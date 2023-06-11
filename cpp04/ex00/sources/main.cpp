0c6f680f93f94cc8e4785e715635dafbba13ef23
Rodrigo Lins <rlinsdev@gmail.com>
2023-06-11 11:55:00 -0300

    Main - Cat Test

--- cpp04/ex00/sources/main.cpp
+++ cpp04/ex00/sources/main.cpp
@@ -6,30 +6,44 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/09 07:40:22 by rlins             #+#    #+#             */
-/*   Updated: 2023/06/11 11:38:55 by rlins            ###   ########.fr       */
+/*   Updated: 2023/06/11 11:53:44 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <Animal.hpp>
 #include <color.hpp>
 #include <Dog.hpp>
+#include <Cat.hpp>
 #include <iostream>
 
 void	BaseClass();
 void	dogByDerivedDogClass();
 void	dogByBaseClass();
+void	catByDerivedCatClass();
+void	catByBaseClass();
 
 int	main(void) {
-
 	BaseClass();
+
+	// Dog tests
+	std::cout << std::endl;
 	dogByDerivedDogClass();
 	dogByBaseClass();
+
+	// Cat tests
+	std::cout << std::endl;
+	catByDerivedCatClass();
+	catByBaseClass();
 
 	return (0);
 }
 
+/**
+ * @brief Tests with base class
+ *
+ */
 void	BaseClass() {
-	std::cout << std::endl << BG_WHITE WHITE "Animal - Base class tests" RESET << std::endl;
+	std::cout << BG_WHITE WHITE "Animal - Base class tests" RESET << std::endl;
 	Animal *animal = new Animal();
 	std::cout << GREEN "Animal type: " << animal->getType() << RESET << std::endl;
 	animal->makeSound();
@@ -37,8 +51,12 @@ void	BaseClass() {
 	std::cout << std::endl;
 }
 
+/**
+ * @brief Dog instantiate class, but by Dog class /Derived class
+ *
+ */
 void dogByDerivedDogClass() {
-	std::cout << std::endl << BG_WHITE WHITE "Dog by derived Dog class" RESET << std::endl;
+	std::cout << BG_WHITE WHITE "Dog by derived Dog class" RESET << std::endl;
 	Dog *dog = new Dog();
 	std::cout << GREEN << "Dog type: " << dog->getType() << RESET << std::endl;
 	dog->makeSound();
@@ -46,11 +64,39 @@ void dogByDerivedDogClass() {
 	std::cout << std::endl;
 }
 
+/**
+ * @brief Dog instantiate class,but by base class
+ */
 void dogByBaseClass() {
-	std::cout << std::endl << BG_WHITE WHITE "Dog by Animal base class" RESET << std::endl;
+	std::cout << BG_WHITE WHITE "Dog by Animal base class" RESET << std::endl;
 	Animal *dog = new Dog();
 	std::cout << GREEN << "Animal type: " << dog->getType() << RESET << std::endl;
 	dog->makeSound();
 	delete dog;
+	std::cout << std::endl;
+}
+
+/**
+ * @brief Cat instantiate class, but by Cat class /Derived class
+ *
+ */
+void catByDerivedCatClass() {
+	std::cout << BG_WHITE WHITE "Cat by derived Cat class" RESET << std::endl;
+	Cat *cat = new Cat();
+	std::cout << GREEN << "Cat type: " << cat->getType() << RESET << std::endl;
+	cat->makeSound();
+	delete cat;
+	std::cout << std::endl;
+}
+
+/**
+ * @brief Cat instantiate class,but by base class
+ */
+void catByBaseClass() {
+	std::cout << BG_WHITE WHITE "Cat by Animal base class" RESET << std::endl;
+	Animal *cat = new Cat();
+	std::cout << GREEN << "Animal type: " << cat->getType() << RESET << std::endl;
+	cat->makeSound();
+	delete cat;
 	std::cout << std::endl;
 }
