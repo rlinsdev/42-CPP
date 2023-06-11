29fcb6abae8e9e8bf56c5ba703bb73625d1e8d7f
Rodrigo Lins <rlinsdev@gmail.com>
2023-06-11 12:58:54 -0300

    Main Tests

--- cpp04/ex00/sources/main.cpp
+++ cpp04/ex00/sources/main.cpp
@@ -6,15 +6,17 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/09 07:40:22 by rlins             #+#    #+#             */
-/*   Updated: 2023/06/11 11:58:02 by rlins            ###   ########.fr       */
+/*   Updated: 2023/06/11 12:57:01 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include <Animal.hpp>
 #include <color.hpp>
 #include <Dog.hpp>
 #include <Cat.hpp>
-#include <iostream>
+#include <WrongAnimal.hpp>
+#include <WrongCat.hpp>
 
 void	BaseClass();
 void	dogByDerivedDogClass();
@@ -22,6 +24,8 @@ void	dogByBaseClass();
 void	catByDerivedCatClass();
 void	catByBaseClass();
 void	subjectTest();
+void 	wrongTest();
+void	wrongSubjectTest();
 
 int	main(void) {
 	// BaseClass();
@@ -36,7 +40,13 @@ int	main(void) {
 	// catByDerivedCatClass();
 	// catByBaseClass();
 
-	// Suject tests
+	// Wrong Test
+	// WrongTest();
+
+	// Wrong Subject
+	// wrongSubjectTest();
+
+	// Subject tests
 	subjectTest();
 
 	return (0);
@@ -105,6 +115,9 @@ void catByBaseClass() {
 	std::cout << std::endl;
 }
 
+/**
+ * @brief Identical test from subject
+ */
 void	subjectTest() {
 	std::cout << BG_WHITE WHITE "Subject test!" RESET << std::endl;
 	const Animal* meta = new Animal();
@@ -120,4 +133,51 @@ void	subjectTest() {
 	delete i;
 	delete meta;
 	std::cout << std::endl;
+}
+
+/**
+ * @brief Test from subject - Wrong call
+ */
+void	wrongSubjectTest() {
+	std::cout << BG_WHITE WHITE "[Wrong] Subject test!" RESET << std::endl;
+	const WrongAnimal* meta = new WrongAnimal();
+	const Animal* j = new Dog();
+	const WrongAnimal* i = new WrongCat();
+
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+	i->makeSound(); //will output the cat sound!
+	j->makeSound();
+	meta->makeSound();
+	delete j;
+	delete i;
+	delete meta;
+	std::cout << std::endl;
+}
+
+void 	wrongTest() {
+	{
+		std::cout << BG_WHITE WHITE "Wrong Animal" RESET << std::endl;
+		const WrongAnimal *	animal = new WrongAnimal();
+		std::cout << "WrongAnimal type: " << animal->getType() << std::endl;
+		animal->makeSound();
+		delete animal;
+		std::cout << std::endl;
+	}
+	{
+		std::cout << BG_WHITE WHITE "Wrong Cat By Wrong Cat Class" RESET << std::endl;
+		const WrongCat *	cat = new WrongCat();
+		std::cout << "WrongCat type: " << cat->getType() << std::endl;
+		cat->makeSound();
+		delete cat;
+		std::cout << std::endl;
+	}
+	{
+		std::cout << BG_WHITE WHITE "WrongCat By WrongAnimal Class" RESET << std::endl;
+		const WrongAnimal * cat = new WrongCat();
+		std::cout << "WrongAnimal type: " << cat->getType() << std::endl;
+		cat->makeSound();
+		delete cat;
+		std::cout << std::endl;
+	}
 }
