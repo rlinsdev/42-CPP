0e74e833d3c997129008f227b835c641c2d71063
Rodrigo Lins <rlinsdev@gmail.com>
2023-06-20 07:52:33 -0300

    main tests

--- cpp05/ex00/sources/main.cpp
+++ cpp05/ex00/sources/main.cpp
@@ -6,36 +6,63 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/04 07:40:47 by rlins             #+#    #+#             */
-/*   Updated: 2023/06/18 10:36:49 by rlins            ###   ########.fr       */
+/*   Updated: 2023/06/20 07:46:59 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <Bureaucrat.hpp>
+#include <Color.hpp>
 #include <iostream>
+
+void	createBureaucratTest(std::string name, int grade);
+void 	defaultConstructor(void);
+void	nameGradeConstructor(void);
 
 int	main(void) {
 
-	// std::string rb1 = "Robot-01";
-	// std::string rb2 = "Android-02";
-
-	// ClapTrap ct1 (rb1);
-	// ClapTrap ct2 (rb2);
-
-	// std::cout << std::endl << "Test: " << rb1 << " Attack" << std::endl;
-	// ct1.setAttackDamage(1);
-	// ct1.attack(rb2);
-	// ct2.takeDamage(ct1.getAttackDamage());
-
-	// std::cout << std::endl << "Test: " << rb2 << " Attack" << std::endl;
-	// ct2.setAttackDamage(2);
-	// ct2.attack(rb1);
-	// ct1.takeDamage(ct2.getAttackDamage());
-
-	// std::cout << std::endl << "Test: Repaired all" << std::endl;
-	// ct1.beRepaired(2);
-	// ct2.beRepaired(1);
-
-	// std::cout << std::endl << "End of simulation..." << std::endl;
+	// defaultConstructor();
+	nameGradeConstructor();
 
 	return (0);
+}
+
+/**
+ * @brief Test to check the CreateBureaucrat by Name and grade
+ */
+void	nameGradeConstructor(void) {
+	std::cout << std::endl << BG_WHITE BLACK "Bureaucrat Name and Grade constructor" RESET << std::endl;
+	createBureaucratTest("Michael", 20);
+	createBureaucratTest("Rick", 150);
+	createBureaucratTest("Rock", -10);
+	createBureaucratTest("Mustang", 200);
+}
+
+/**
+ * @brief Test by default constructor to Bureaucrat
+ */
+void	defaultConstructor(void) {
+	std::cout << std::endl << BG_WHITE BLACK "Bureaucrat default constructor" RESET << std::endl;
+	Bureaucrat	bureaucrat;
+	std::cout << "Default bureaucrat is: " << bureaucrat << std::endl;
+}
+
+/**
+ * @brief Create a Bureaucrat Test. Protected code by Try Catch Statements
+ * @param name of Bureaucrat
+ * @param grade by Bureaucrat
+ */
+void	createBureaucratTest(std::string name, int grade) {
+	std::cout << "Creating bureaucrat named " << BLUE
+		<< name << RESET << " with grade " << BLUE << grade << RESET << ":" << std::endl;
+	try {
+		Bureaucrat bureaucrat(name, grade);
+		std::cout << bureaucrat << " successfully created." << std::endl;
+		return ;
+	}
+	catch(Bureaucrat::GradeTooHighException & e) {
+		std::cerr << RED "Exception: " << e.what() << RESET << std::endl;
+	}
+	catch(Bureaucrat::GradeTooLowException & e) {
+		std::cerr << RED "Exception: " << e.what() << RESET << std::endl;
+	}
 }
